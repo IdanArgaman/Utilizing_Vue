5147d816c1921f66fa2319fe0b09b8c5e3a73107
Idan <IdanArgaman@gmail.com>
2021-05-05 18:13:50 +0300

    Add parent control

---
+++ src/pages/basics/parent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Address v-bind="data" />
+    {{ data }}
+  </div>
+</template>
+
+<script>
+import Address from "./address";
+
+export default {
+  props: {
+      data: {
+          type: Object,
+          default: () => {}
+      }
+  },
+  components: {
+    Address,
+  },
+  mounted() {
+      console.log('Parent mounted!')
+  }
+};
+</script>
+
+<style>
+</style>
