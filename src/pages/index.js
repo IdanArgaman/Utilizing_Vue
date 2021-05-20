390fed13465dcbe428bdb837fb311be2f1324632
Idan <IdanArgaman@gmail.com>
2021-05-20 09:59:18 +0300

    More refactoring

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,13 @@
-import vueBasicsPage from './basics/page.vue';
-import vueBasicSlotsPage from './basic-slots/page.vue';
-import vueNestedRoutesPage from './advanced-routing/nested/page.vue';
-import vueNameRoutesPage from './advanced-routing/named/page.vue';
-import axiosHocPage from './axios-hoc/page.vue';
-import vueRenderlessPage from './renderless/page.vue';
-import vueTabPage from './tabs/page.vue';
-import vueRuesableModalPage from './modal/page.vue';
-import vueListenersAndAttrs from './listenersAndAttrs/page.vue';
-import vueNestedVModelPage from './nested-v-model/page.vue';
-import vueDynamicFormPage from './dynamic-form/page.vue';
-import vueRenderPage from './render/page.vue';
-import vueCustomEventsPage from './customEvents/page.vue';
-
-export {
-    vueBasicsPage,
-    vueBasicSlotsPage,
-    vueNestedRoutesPage,
-    vueNameRoutesPage,
-    axiosHocPage,
-    vueRenderlessPage,
-    vueTabPage,
-    vueRuesableModalPage,
-    vueListenersAndAttrs,
-    vueNestedVModelPage,
-    vueDynamicFormPage,
-    vueRenderPage,
-    vueCustomEventsPage
-}
+export { default as vueBasics } from './basics/page.vue';
+export { default as vueBasicSlotsPage } from './basic-slots/page.vue';
+export { default as vueNestedRoutesPage } from './advanced-routing/nested/page.vue';
+export { default as vueNameRoutesPage } from './advanced-routing/named/page.vue';
+export { default as axiosHocPage } from './axios-hoc/page.vue';
+export { default as vueRenderlessPage } from './renderless/page.vue';
+export { default as vueTabPage } from './tabs/page.vue';
+export { default as vueRuesableModalPage } from './modal/page.vue';
+export { default as vueListenersAndAttrs } from './listenersAndAttrs/page.vue';
+export { default as vueNestedVModelPage } from './nested-v-model/page.vue';
+export { default as vueDynamicFormPage } from './dynamic-form/page.vue';
+export { default as vueRenderPage } from './render/page.vue';
+export { default as vueCustomEventsPage } from './customEvents/page.vue';
